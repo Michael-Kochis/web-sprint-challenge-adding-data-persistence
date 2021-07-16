f2ccba9091d2f566be3da0c9ca47d7ce9974156f
Michael-Kochis <rhishisikk@gmail.com>
2021-07-16 12:26:06 -0500

    corrections to draw project values into task

--- api/task/model.js
+++ api/task/model.js
@@ -1,11 +1,14 @@
 const db = require('../../data/dbConfig')
 
 async function find() {
-    let returnThis = await db('tasks');
+    let returnThis = await db('tasks as t')
+        .join('projects as p', 't.project_id', 'p.project_id')
+        .select('task_id', 'task_description', 'task_notes', 'task_completed', 
+            'project_name', 'project_description');
     returnThis.forEach((task) => {
         task.task_completed = !!task.task_completed;
     })
-
+    
     return returnThis;
 }
 
