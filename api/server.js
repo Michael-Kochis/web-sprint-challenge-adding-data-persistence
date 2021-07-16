60e8dade14679e9cb1fee08e68a9e9240b87c276
Michael-Kochis <rhishisikk@gmail.com>
2021-07-16 10:41:33 -0500

    basic server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,33 @@
-// build your server here and require it from index.js
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const ProjectRouter = require('./project/router');
+const ResourceRouter = require('./resource/router');
+const TaskRouter = require('./task/router');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors() );
+server.use(helmet() );
+
+server.use('/api/projects', ProjectRouter);
+server.use('/api/resources', ResourceRouter);
+server.use('/api/tasks', TaskRouter);
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: "no such endpoint" })
+})
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      sageAdvice: 'Finding the real error is 90% of the bug fix',
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+  
+  
+
+module.exports = server;
